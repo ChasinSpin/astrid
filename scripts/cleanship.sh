acac7007ef27fb275b6380a5d9f84cfbf5a1f95c
ChasinSpin <chasinspin@icloud.com>
2024-09-06 05:41:11 +0000

    Cleanship script (for Ted) now reactivates the privacy notic

--- scripts/cleanship.sh
+++ scripts/cleanship.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-echo "Clean Astrid to make it ready to ship (includes removing home Wifi)"
+echo "Clean Astrid to make it ready to ship (includes removing home Wifi, and reenabling privacy notice)"
 echo "** Astrid will AUTOMATICALLY POWER DOWN after completion **"
 echo -n "Proceed? (y/n) > "
 read line
 if [ "$line" = "y" -o "$line" = "Y" ]; then
 	echo -n "Cleaning..."
-	/usr/bin/rm /home/pi/.bash_history
 
 	TMP_FILE=/tmp/cleanship.$$
+
+	if [ -d /media/pi/ASTRID ];then
+		HIDDEN=/media/pi/ASTRID/configs/hidden.json
+		/usr/bin/grep -v "privacy_notice" $HIDDEN > "$TMP_FILE"
+		/usr/bin/cat "$TMP_FILE" > $HIDDEN
+	fi
+
+	/usr/bin/rm /home/pi/.bash_history
+
 /usr/bin/cat << EOF >$TMP_FILE
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
